574500b16d1144e7dd0e72b3479621a928fe8f24
eman <emankamalnasr96@gmail.com>
2021-10-25 03:32:57 +0200

    setup navigation

--- app/src/main/java/com/example/redditposts/MainActivity.kt
+++ app/src/main/java/com/example/redditposts/MainActivity.kt
@@ -1,38 +1,11 @@
 package com.example.redditposts
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.redditposts.ui.theme.RedditPostsTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            RedditPostsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RedditPostsTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
